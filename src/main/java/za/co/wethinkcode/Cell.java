db39a7949337a421c38d14e3ea53c348fec79fc6
ljonas021 <ljonas021@student.wethinkcode.co.za>
2022-08-02 20:24:05 +0200

    displays cells

--- src/main/java/za/co/wethinkcode/Cell.java
+++ src/main/java/za/co/wethinkcode/Cell.java
@@ -7,17 +7,36 @@ public class Cell {
 
     private int pos_x, pos_y;    
 
-    int w = ConwayPanel.WIDTH / ConwayPanel.COLS, h = ConwayPanel.HEIGHT / ConwayPanel.ROWS;
+    private int w = ConwayPanel.WIDTH / ConwayPanel.COLS;
+    private int h = ConwayPanel.HEIGHT / ConwayPanel.ROWS;
+
+    private int isAlive;
 
     public Cell(int pos_x, int pos_y){
         this.pos_x = pos_x;
         this.pos_y = pos_y;
+        isAlive = 0;
     }
 
     public void draw_cell(Graphics g, Color c){
-        g.setColor(c);
-        g.fillRect(pos_x * w, pos_y * h, w, h);
-        g.setColor(Color.black);
-        g.drawRect(pos_x * w, pos_y * h, w, h);
+        if (isAlive == 1) {
+            g.setColor(c);
+            g.fillRect(pos_x * w, pos_y * h, w, h);
+            g.setColor(Color.black);
+            g.drawRect(pos_x * w, pos_y * h, w, h);
+        } else { 
+            g.setColor(Color.white);
+            g.fillRect(pos_x * w, pos_y * h, w, h);
+            g.setColor(Color.black);
+            g.drawRect(pos_x * w, pos_y * h, w, h);
+        }
     }    
+
+    public int getIsAlive() {
+        return isAlive;
+    }
+
+    public void setIsAlive(int isAlive) {
+        this.isAlive = isAlive;
+    }
 }
--- src/main/java/za/co/wethinkcode/ConwayPanel.java
+++ src/main/java/za/co/wethinkcode/ConwayPanel.java
@@ -1,17 +1,82 @@
 package za.co.wethinkcode;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Graphics;
+import java.awt.Color;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class ConwayPanel extends JPanel{
+public class ConwayPanel extends JPanel implements ActionListener{
 
     static final int WIDTH = 500, HEIGHT = 500;
     static final int ROWS = 20, COLS = 20;
 
+    private final int DELAY = 100;
+
+    private Cell [][] grid;
+
+    private boolean isRunning = false;
+
+    private Timer timer;
 
     public ConwayPanel() {
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.MAGENTA);
+        this.setFocusable(true);
+
+        grid = new Cell[ROWS][COLS];
+
+        startGame();
+        createGrid();
+
+        grid[5][5].setIsAlive(1);
+        grid[6][6].setIsAlive(1);
+        grid[6][7].setIsAlive(1);
+        grid[7][6].setIsAlive(1);
+        grid[5][7].setIsAlive(1);
     }
 
+
+    private void createGrid() {
+        for (int i = 0; i < COLS; i++) {
+            for (int j = 0; j < ROWS; j++) {
+                grid[j][i] = new Cell(j, i);
+            }
+        }
+    }
+
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                grid[j][i].draw_cell(g, Color.GREEN);
+            }
+        }
+    }
+
+
+    public void stopGame(){
+        isRunning = false;
+        timer.stop();
+    }
+
+
+    public void startGame(){
+        isRunning = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+
+        repaint();
+    }
 }
